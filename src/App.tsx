647c9eda4be352f047577534672273d539379056
MohamedSaid7102 <mohamedshelfwork@gmail.com>
2022-12-12 08:20:24 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+function App() {
+  return <div className="">Hello world</div>;
+}
+
+export default App;
+
---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+import path from 'node:path';
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@redux': path.resolve(__dirname, './src/redux'),
+      '@router': path.resolve(__dirname, './src/router'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@myTypes': path.resolve(__dirname, './types'),
+    },
+  },
+});
+
