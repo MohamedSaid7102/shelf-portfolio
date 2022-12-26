51fc546f19f89b02e8344a9286ee460159275f2e
MohamedSaid7102 <mohamedshelfwork@gmail.com>
2022-12-27 01:48:57 +0200

    Added Stackoverflow link to Social links

--- src/data/socialLinksData.tsx
+++ src/data/socialLinksData.tsx
@@ -193,6 +193,74 @@ export const socialLinksData: socialLinksDataType = [
     ),
   },
   {
+    title: 'Stackoverflow',
+    url: 'https://stackoverflow.com/users/12854478/mohamed-s-shelf',
+    logo: (
+      <svg
+        aria-hidden={true}
+        xmlns="http://www.w3.org/2000/svg"
+        width="45"
+        height="45"
+        fill="currentColor"
+        viewBox="0 0 256 256"
+      >
+        <rect width="256" height="256" fill="none"></rect>
+        <polyline
+          points="48 152 48 216 208 216 208 152"
+          fill="none"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="16"
+        ></polyline>
+        <line
+          x1="88"
+          y1="176"
+          x2="168"
+          y2="176"
+          fill="none"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="16"
+        ></line>
+        <line
+          x1="95"
+          y1="123.2"
+          x2="172.2"
+          y2="143.9"
+          fill="none"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="16"
+        ></line>
+        <line
+          x1="115.3"
+          y1="74"
+          x2="184.6"
+          y2="114"
+          fill="none"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="16"
+        ></line>
+        <line
+          x1="147.8"
+          y1="31.8"
+          x2="204.3"
+          y2="88.3"
+          fill="none"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="16"
+        ></line>
+      </svg>
+    ),
+  },
+  {
     title: 'Telegram',
     url: 'https://telegram.me/Mohamed_S_Shelf',
     logo: (
